66aa08f78a999bb7d411c1586c0b8f4dd3c7236e
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-05-20 23:05:22 +0530

    Time: 357 ms (5.43%) | Memory: 61.1 MB (44.59%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,7 +4,10 @@ public:
         priority_queue<int, vector<int>, greater<int>> minHeap;
         for(int num :nums){
             minHeap.push(num);
+            cout<<minHeap.top()<<" ";
+
             if(minHeap.size() > k){
+                 cout<<"size "<<minHeap.top()<<" ";
                 minHeap.pop();
             }
         }
