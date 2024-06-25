542d7f308ff403f4d0b88b37758b0e1a2e256b46
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-06-25 12:53:19 +0530

    Time: 75 ms (85.51%) | Memory: 58.3 MB (91.30%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+    let sum =0 ;
+    const solution =(node)=>{
+        if(node){
+            solution(node.right);
+            sum+= node.val;
+            node.val = sum;
+            solution(node.left);
+        }
+    };
+    solution(root);
+    return root;
+    
+};
