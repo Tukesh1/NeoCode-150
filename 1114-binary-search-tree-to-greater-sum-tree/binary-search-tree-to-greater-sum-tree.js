5cd1c70800eab7128cb8d532390a74b3b62cdd38
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-06-25 12:48:50 +0530

    Time: 50 ms (77.82%) | Memory: 49.3 MB (53.92%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var bstToGst = function (root) {
+    
+    let sum = 0;
+    const solve = (node)=>{
+        if(node){
+        solve(node.right);
+        sum += node.val ;
+        node.val= sum;
+        solve(node.left);
+        }
+    };
+    solve(root);
+    return root;
+};
