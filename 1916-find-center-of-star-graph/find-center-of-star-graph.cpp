07f6cacc71b14f68f547d0249b0c93b8730410a4
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-06-27 13:19:10 +0530

    Time: 124 ms (89.29%) | Memory: 71.7 MB (40.94%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        bitset<100000> visited = 0;
+        for (auto& e : edges) {
+            int v = e[0];
+            int w = e[1];
+            if (visited[v])
+                return v;
+            if (visited[w])
+                return w;
+            visited[v] = visited[w] = 1;
+        }
+        return -1;
+    }
+};
