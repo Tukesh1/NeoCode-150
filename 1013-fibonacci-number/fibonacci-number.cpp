3941454ccec9478e5f2e94cf26ff9a5fa4a46906
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-09-17 02:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (78.50%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,13 +6,22 @@ public:
     //    return fib(n-1)+fib(n-2) ;
     // DP solution 
 
-    vector<int>dp(n+1);
-    if(n==0) return 0;
-    dp[0]=0;
-    dp[1]=1;
-    for(int i= 2;i<=n;i++){
-        dp[i]= dp[i-1]+dp[i-2];
-    }
-    return dp[n];
+    // vector<int>dp(n+1);
+    if(n<=1) return n;// base case
+    // dp[0]=0;
+    // dp[1]=1;
+    // for(int i= 2;i<=n;i++){
+    //     dp[i]= dp[i-1]+dp[i-2];
+    // }
+    // return dp[n];
+      
+      int a= 0, b=1;
+      int c=0;
+      for(int i=1;i<n;i++){
+        c= a+b;
+        a=b;
+        b=c;
+      }
+      return c;
     }
 };
