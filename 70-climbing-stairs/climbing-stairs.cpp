b6c2d9024284f07b6405456b93f4ef0cafec52fb
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-09-17 19:18:36 +0530

    Time: 2 ms (49.14%) | Memory: 7.4 MB (69.71%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     int climbStairs(int n) {
-       // dp
+        if (n == 0 || n == 1 || n == 2)
+            return n;
+        int a=1, b=2;
+        int c;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};
+/*
+ //recursion
+        if(n<0) return 0;// not a way to climb
+        if(n==0) return 1;// got one way to climb
+        int OneStep = climbStairs(n-1);
+        int twoStep = climbStairs(n-2);
+        return OneStep+ twoStep;
+
+        // dp // memorisation
        vector<int>dp(n+1);
        if(n==1) return 1;
        dp[0]=0;
@@ -11,14 +31,4 @@ public:
         dp[i]= dp[i-1]+dp[i-2];
        }
        return dp[n];
-
-    }
-};
-/*
- //recursion 
-        if(n<0) return 0;// not a way to climb
-        if(n==0) return 1;// got one way to climb
-        int OneStep = climbStairs(n-1);
-        int twoStep = climbStairs(n-2);
-        return OneStep+ twoStep;
 */
